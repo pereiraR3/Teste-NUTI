ae214eba0e47f11cd7de9e0e82c37c23a1803787
pereira3r <anthony_rodriguespereira@outlook.com>
2024-08-31 13:48:56 -0400

    fix: edit properties & edit DTOs

--- api-pncp/src/main/java/com/example/api_pncp/Model/Orgao/OrgaoResponseDTO.java
+++ api-pncp/src/main/java/com/example/api_pncp/Model/Orgao/OrgaoResponseDTO.java
@@ -1,14 +1,27 @@
 package com.example.api_pncp.Model.Orgao;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
- * DTO criado para retornar os dados sobre determinada "Pesquisa" requerida pelo FrontEnd, isso de acordo com os
- * dados que já foram persistidos.
+ * DTO criado para retornar os dados sobre determinado órgão relacionado a uma pesquisa.
  */
 
+@Schema(description = "DTO para retornar os dados de um órgão relacionado a uma pesquisa.")
 public record OrgaoResponseDTO(
 
-    Long id, 
+    @Schema(description = "Identificador único do órgão.", example = "1")
+    Long id,
+
+    @Schema(description = "CNPJ do órgão.", example = "12345678000195")
     String cnpj
 
 ) {
-} 
+
+    public OrgaoResponseDTO(Orgao orgao) {
+        this(
+            orgao.getId(),
+            orgao.getCnpj()
+        );
+    }
+
+}
